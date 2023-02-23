3e9f98626cb08bc3c737d0d336ee7736024a8dbf
Jon Naske <jonnaske@hotmail.com>
2023-02-22 23:14:11 -0800

    social icons adjusted

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -17,48 +17,48 @@ export default function Socials(props: Props) {
   return (
     <div className="flex flex-wrap justify-items-end gap-1">
       {props.facebookUrl && (
-        <Link href={props.facebookUrl} className="btn-xl  btn">
-          <FaFacebook className="scale-[2]" />
+        <Link href={props.facebookUrl} className="btn-ghost btn-sm btn">
+          <FaFacebook className="scale-[1.75]" />
         </Link>
       )}
       {props.instagramUrl && (
-        <Link href={props.instagramUrl} className="btn-xl  btn">
-          <FaInstagram className="scale-[2]" />
+        <Link href={props.instagramUrl} className="btn-ghost btn-sm btn">
+          <FaInstagram className="scale-[1.75]" />
         </Link>
       )}
       {props.twitterUrl && (
-        <Link href={props.twitterUrl} className="btn-xl  btn">
-          <FaTwitter className="scale-[2]" />
+        <Link href={props.twitterUrl} className="btn-ghost btn-sm btn">
+          <FaTwitter className="scale-[1.75]" />
         </Link>
       )}
       {props.youtubeUrl && (
-        <Link href={props.youtubeUrl} className="btn-xl  btn">
-          <FaYoutube className="scale-[2]" />
+        <Link href={props.youtubeUrl} className="btn-ghost btn-sm btn">
+          <FaYoutube className="scale-[1.75]" />
         </Link>
       )}
       {props.linkedInUrl && (
-        <Link href={props.linkedInUrl} className="btn-xl  btn">
-          <FaLinkedin className="scale-[2]" />
+        <Link href={props.linkedInUrl} className="btn-ghost btn-sm btn">
+          <FaLinkedin className="scale-[1.75]" />
         </Link>
       )}
       {props.pinterestUrl && (
-        <Link href={props.pinterestUrl} className="btn-xl  btn">
-          <FaPinterest className="scale-[2]" />
+        <Link href={props.pinterestUrl} className="btn-ghost btn-sm btn">
+          <FaPinterest className="scale-[1.75]" />
         </Link>
       )}
       {props.tiktokUrl && (
-        <Link href={props.tiktokUrl} className="btn-xl  btn">
-          <FaTiktok className="scale-[2]" />
+        <Link href={props.tiktokUrl} className="btn-ghost btn-sm btn">
+          <FaTiktok className="scale-[1.75]" />
         </Link>
       )}
       {props.snapchatUrl && (
-        <Link href={props.snapchatUrl} className="btn-xl  btn">
-          <FaSnapchat className="scale-[2]" />
+        <Link href={props.snapchatUrl} className="btn-ghost btn-sm btn">
+          <FaSnapchat className="scale-[1.75]" />
         </Link>
       )}
       {props.whatsappUrl && (
-        <Link href={props.whatsappUrl} className="btn-xl  btn">
-          <FaWhatsapp className="scale-[2]" />
+        <Link href={props.whatsappUrl} className="btn-ghost btn-sm btn">
+          <FaWhatsapp className="scale-[1.75]" />
         </Link>
       )}
     </div>
