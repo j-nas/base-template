5d766316ffef7c44b44348dd869fe878e6b453a5
Jon Naske <jonnaske@hotmail.com>
2023-02-24 16:15:28 -0800

    added dynamic import for service page images

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -5,25 +5,20 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { createInnerTRPCContext } from "../../server/api/trpc";
 import { appRouter } from "../../server/api/root";
 import Link from "next/link";
-
 const TopHero = dynamic(() => import("../../components/TopHero"), {
   loading: () => <p>Loading...</p>,
 });
-const CldImg = dynamic(() => import("../../components/CldImg"), {
-  loading: () => <p>Loading...</p>,
-});
-const Footer = dynamic(() => import("../../components/Footer"), {
-  loading: () => <p>Loading...</p>,
-});
+const CldImage = dynamic(() =>
+  import("next-cloudinary").then((mod) => mod.CldImage)
+);
+const Footer = dynamic(() => import("../../components/Footer"));
 const HeroBanner = dynamic(() => import("../../components/BottomHero"), {
   loading: () => <p>Loading...</p>,
 });
 const Navbar = dynamic(() => import("../../components/Navbar"), {
   loading: () => <p>Loading...</p>,
 });
-const Markdown = dynamic(() => import("../../components/Markdown"), {
-  loading: () => <p>Loading...</p>,
-});
+
 export const Services: NextPage<
   InferGetStaticPropsType<typeof getStaticProps>
 > = (props) => {
@@ -39,25 +34,25 @@ export const Services: NextPage<
         <Navbar business={business} services={services} />
         <TopHero pageTitle="Our Services" hero={topHero} />
 
-        <section className="container mx-auto mt-32 grid w-11/12 place-items-stretch gap-12 lg:grid-cols-2">
+        <section className="container mx-auto mt-32 grid w-11/12 place-items-stretch gap-12 ">
           {services.map((service) => (
-            <div
-              key={service.id}
-              className="card bg-base-100 shadow-xl md:card-side"
-            >
-              <figure className="min-h-fit">
-                <CldImg
+            <div key={service.id} className="card md:card-side">
+              <figure className="w-full md:max-w-xs">
+                <CldImage
                   alt={service.shortDescription}
-                  blur={service.PrimaryImage.blur_url}
                   format={service.PrimaryImage.format}
-                  public_Id={service.PrimaryImage.public_Id}
-                  height={800}
-                  width={800}
-                  id={service.PrimaryImage.id}
-                  className="h-full  object-none transition-transform hover:scale-110"
+                  src={`${process.env.NEXT_PUBLIC_CLOUDINARY_FOLDER}/${service.PrimaryImage.public_Id}`}
+                  height="1600"
+                  width="1600"
+                  placeholder="blur"
+                  blurDataURL={service.PrimaryImage.blur_url}
+                  gravity="auto"
+                  crop="thumb"
+                  className=" h-full object-cover transition-transform hover:scale-110"
+                  sizes="100vw, 1280px"
                 />
               </figure>
-              <div className="card-body w-full">
+              <div className="card-body ">
                 <h2 className="card-title">{service.title}</h2>
                 <p>{service.shortDescription}</p>
                 <div className="card-actions justify-end">
