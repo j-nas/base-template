63bae6e2191d0a046ca0e171e48af2d6fbb2857b
Jon Naske <jonnaske@hotmail.com>
2023-02-27 22:59:43 -0800

    changed image relations to 1-1

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,38 +1,13 @@
 import { type Testimonial } from "@prisma/client";
 import Image from "next/image";
 import { env } from "../env/client.mjs";
+import { type RouterOutputs } from "../utils/api";
 
-type Props = {
-  testimonial: {
-    AvatarImage?:
-      | {
-          image: {
-            public_Id: string;
-            format: string;
-            id: string;
-            width: number;
-            height: number;
-          };
-          id: string;
-        }[]
-      | undefined;
-    id: string;
-    name: string;
-    title: string;
-    quote: string;
-    highlighted: boolean;
-  };
-};
+type Props = RouterOutputs["testimonial"]["getOne"];
 
 export default function Testimonial(props: Props) {
-  const {
-    testimonial: { id, name, title, quote, AvatarImage = [] },
-  } = props;
-
-  const { public_Id, format } = AvatarImage[0]?.image ?? {
-    format: "",
-    public_Id: "",
-  };
+  const { id, name, image, company, quote, title } = props;
+  const { public_Id, format } = image ?? {};
 
   const placeholder = (
     <div className="placeholder avatar absolute -top-8 left-8">
@@ -59,20 +34,23 @@ export default function Testimonial(props: Props) {
   return (
     <div
       key={id}
-      className="relative mx-8 mt-12 rounded-lg bg-base-300 p-6 shadow-2xl md:mx-auto md:w-1/2 lg:w-full"
+      className="relative mx-auto  mt-12 w-full rounded-lg bg-base-300 p-6 shadow-2xl"
     >
-      {public_Id !== "" ? avatar : placeholder}
-      <div className="grid grid-cols-2 ">
+      {image !== null ? avatar : placeholder}
+      <div className="grid grid-cols-2 grid-rows-3  sm:grid-rows-none">
         <p className="col-span-2 border-b-2 border-secondary pb-4 pt-8">
           {quote}
         </p>
-        <div className="">
-          <div className="flex flex-col  text-left">
-            <span className="font-medium">{name}</span>
-            <span className="text-base">{title}</span>
+        <div className="col-span-2 self-stretch sm:col-span-1">
+          <div className="flex flex-col text-left">
+            <span className="font-normal text-base">{name}</span>
+            <span className="font-light text-sm after:content-[',']">
+              {company}
+            </span>
+            <span className="font-light text-sm">{title}</span>
           </div>
         </div>
-        <div className="flex justify-end ">
+        <div className="col-span-2 row-start-3 flex max-h-fit justify-center p-0 sm:col-span-1 sm:row-start-auto sm:justify-end ">
           <div className="mask mask-star w-6 bg-warning text-base"></div>
           <div className="mask mask-star w-6 bg-warning text-base"></div>
           <div className="mask mask-star w-6 bg-warning text-base"></div>
