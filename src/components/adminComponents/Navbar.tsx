fcbdbea120c7a79fa4aa450305aef581d0287d3b
Jon Naske <jonnaske@hotmail.com>
2023-03-08 17:49:49 -0800

    hamburger button added to navbar

--- src/components/adminComponents/Navbar.tsx
+++ src/components/adminComponents/Navbar.tsx
@@ -1,22 +1,17 @@
 import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 import Link from "next/link";
 import Logo from "../Logo";
-import dynamic from "next/dynamic";
 import DarkModeButton from "../DarkModeButton";
-// import HamburgerMenu from "./HamburgerMenu";
-import { type BusinessInfo } from "@prisma/client";
-import { type RouterOutputs } from "../../utils/api";
 import { useSession } from "next-auth/react";
+import HamburgerButton from "./HamburgerButton";
 
 type Props = {
-  services: RouterOutputs["service"]["getActive"];
-  business: RouterOutputs["businessInfo"]["getActive"];
   toggleSidebar: () => void;
+  sidebarOpen: boolean;
 };
 
-export default function Navbar({ services, business, toggleSidebar }: Props) {
+export default function Navbar({ sidebarOpen, toggleSidebar }: Props) {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
   const session = useSession();
@@ -27,10 +22,11 @@ export default function Navbar({ services, business, toggleSidebar }: Props) {
 
   return (
     <div className="navbar absolute z-20  bg-base-300 shadow-xl">
-      <div className="navbar-start  md:block">
-        <button onClick={toggleSidebar} className="btn-secondary btn md:hidden">
-          M
-        </button>
+      <div className="navbar-start  ">
+        <HamburgerButton
+          toggleSidebar={toggleSidebar}
+          sidebarOpen={sidebarOpen}
+        />
         <Link
           href="/"
           aria-label="home button logo"
@@ -42,7 +38,7 @@ export default function Navbar({ services, business, toggleSidebar }: Props) {
         </Link>
       </div>
       <div className="navbar-center hidden lg:block">
-        <span className="font-bold  text-lg">Admin Dashboard</span>
+        <span className="font-bold  text-lg">Your Dashboard</span>
       </div>
 
       <div className="mr-2 w-full justify-end md:navbar-end ">
