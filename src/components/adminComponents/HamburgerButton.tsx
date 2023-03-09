ca623df85b091cde1de32ea803849bee6ae3dfe3
Jon Naske <jonnaske@hotmail.com>
2023-03-08 17:49:05 -0800

    hamburger button added to navbar

---
+++ src/components/adminComponents/HamburgerButton.tsx
@@ -0,0 +1,37 @@
+type Props = {
+  toggleSidebar: () => void;
+  sidebarOpen: boolean;
+};
+
+export default function HamburgerButton({ toggleSidebar, sidebarOpen }: Props) {
+  return (
+    <>
+      <label
+        aria-label="nav menu"
+        className={`swap-rotate swap disabled btn-circle btn  pl-4 md:hidden`}
+      >
+        <input type="checkbox" checked={sidebarOpen} onChange={toggleSidebar} />
+
+        <svg
+          className="swap-off -translate-x-2 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512"
+        >
+          <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+        </svg>
+
+        <svg
+          className="swap-on -translate-x-2 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512"
+        >
+          <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+        </svg>
+      </label>
+    </>
+  );
+}
