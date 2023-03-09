20cb0540c9a52841cc7c997e62fbec79fe439428
Jon Naske <jonnaske@hotmail.com>
2023-03-09 12:44:07 -0800

    image dialog

--- src/components/adminComponents/ImageUploadDialog.tsx
+++ src/components/adminComponents/ImageUploadDialog.tsx
@@ -4,13 +4,38 @@ import { api, type RouterOutputs } from "../../utils/api";
 import { IoMdCloseCircle } from "react-icons/io";
 import Link from "next/link";
 import ImageInUseWidget from "./ImageInUseWidget";
+import { Field, Form } from "houseform";
+import { z } from "zod";
+import Image from "next/image";
 type Props = {
   children: React.ReactNode;
+  handleUpload: (input: string) => void;
 };
 
-export default function ImageUploadDialog({ children }: Props) {
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+export default function ImageUploadDialog({ children, handleUpload }: Props) {
+  const [open, setOpen] = React.useState(false);
+  const [base64, setBase64] = React.useState<string | null>(null);
+  const [tempImage, setTempImage] = React.useState<FileList | null>(null);
+
+  const encodeBase64 = (file: File) => {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
+  };
+
   return (
-    <Dialog.Root>
+    <Dialog.Root onOpenChange={() => setBase64(null)}>
       <Dialog.Trigger asChild>{children}</Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 h-screen w-screen bg-black/50 blur-3xl" />
@@ -19,26 +44,104 @@ export default function ImageUploadDialog({ children }: Props) {
             Upload Image
           </Dialog.Title>
           <Dialog.Description className="mt-2 mb-5">
-            Select an image to upload
+            Select an image to upload. Only .jpg, .jpeg, .png and .webp formats.
+            Please keep the size under 5MB.
           </Dialog.Description>
-          <div className="flex flex-col gap-2">
-            <div className="form-control w-full max-w-xs">
-              <label className="label">
-                <span className="label-text">10MB Limit</span>
-              </label>
-              <input
-                type="file"
-                className="file-input-bordered file-input w-full max-w-xs"
-              />
-            </div>
-          </div>
-          <div className="mt-6 flex justify-end">
-            <Dialog.Close asChild>
-              <button aria-label="delete" className="btn-success btn ">
-                Upload
-              </button>
-            </Dialog.Close>
-          </div>
+          <Form
+            onSubmit={(values) => {
+              console.log(values.file[0]);
+              return;
+            }}
+          >
+            {({ submit, errors, getFieldValue }) => (
+              <>
+                <Field
+                  name="file"
+                  onChangeValidate={z
+                    .instanceof(File, { message: "Please select a file" })
+                    .refine((file) => file.size <= MAX_FILE_SIZE, {
+                      message: "File size must be less than 5MB",
+                    })
+                    .refine(
+                      (file) => {
+                        return ACCEPTED_IMAGE_TYPES.includes(file.type);
+                      },
+                      {
+                        message:
+                          "Only .jpg, .jpeg, .png and .webp formats are supported.",
+                      }
+                    )}
+                >
+                  {({ value, setValue, onBlur, errors }) => (
+                    <>
+                      <input
+                        type="file"
+                        className="w-full"
+                        accept="image/*"
+                        onChange={(e) => {
+                          if (!e.target.files) return null;
+                          const file = e.target.files[0];
+
+                          setValue(() => {
+                            return file;
+                          });
+                          if (!file) return null;
+                          encodeBase64(file).then((base64) => {
+                            console.log({ base64 });
+                            setBase64(base64);
+                          });
+                        }}
+                        onBlur={onBlur}
+                        multiple={false}
+                      />
+
+                      <div className="flex flex-col">
+                        {errors &&
+                          errors.map((error) => (
+                            <span key={error} className="text-error">
+                              {error}
+                            </span>
+                          ))}
+                      </div>
+                    </>
+                  )}
+                </Field>
+                <div>
+                  {base64 && (
+                    <div className="relative mt-4 flex h-full w-full place-content-center">
+                      <Image
+                        src={base64}
+                        height={200}
+                        width={200}
+                        alt="preview"
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="mt-6 flex justify-end">
+                  <>
+                    <Dialog.Close asChild>
+                      <button className="btn" aria-label="Cancel" type="button">
+                        Cancel
+                      </button>
+                    </Dialog.Close>
+
+                    <button
+                      onClick={submit}
+                      aria-label="upload"
+                      type="submit"
+                      className={`btn-success btn ml-2 ${
+                        errors.length > 0 && "btn-disabled"
+                      } `}
+                    >
+                      Upload
+                    </button>
+                  </>
+                </div>
+              </>
+            )}
+          </Form>
+
           <Dialog.Close asChild>
             <button
               className="btn-ghost btn-circle btn absolute top-3 right-3"
