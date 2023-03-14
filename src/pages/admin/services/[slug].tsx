2faa963b1eeb8ee2976d9d72188cde8727286dad
Jon Naske <jonnaske@hotmail.com>
2023-03-14 13:43:44 -0700

    image selection dialog implemented

--- src/pages/admin/services/[slug].tsx
+++ src/pages/admin/services/[slug].tsx
@@ -18,7 +18,10 @@ import Layout from "../../../components/adminComponents/Layout";
 import { ReactElement } from "react";
 import { Form, Field, FieldInstance, FormInstance } from "houseform";
 import IconSelectDialog from "../../../components/adminComponents/IconSelectDialog";
+import ImageSelectDialog from "../../../components/adminComponents/ImageSelectDialog";
 import React from "react";
+import { CldImage } from "next-cloudinary";
+import { env } from "../../../env/client.mjs";
 export interface ServicePageQuery extends ParsedUrlQuery {
   slug: Services;
 }
@@ -33,11 +36,24 @@ export const ServiceEditor = () => {
   });
 
   const iconRef = React.useRef<FieldInstance>(null);
+  const primaryImageRef = React.useRef<FieldInstance>(null);
+  const secondaryImageRef = React.useRef<FieldInstance>(null);
+
   const handleIconChange = (value: string) => {
     iconRef.current?.setValue(value);
   };
+  const handleImageChange = (
+    value: string,
+    position: "primary" | "secondary" | "hero"
+  ) => {
+    if (position === "primary") {
+      primaryImageRef.current?.setValue(value);
+    } else {
+      secondaryImageRef.current?.setValue(value);
+    }
+  };
   return (
-    <div className="relative flex h-full w-full flex-col place-items-center overflow-auto">
+    <div className="scroll-bar-thumb-primary relative flex h-full w-full flex-col place-items-center overflow-auto pb-12 scrollbar-thin scrollbar-track-base-200 scrollbar-track-rounded-lg">
       <>
         <Toaster position="bottom-right" />
         {data && (
@@ -59,31 +75,167 @@ export const ServiceEditor = () => {
         </div>
         {data && (
           <div>
-            <div className="flex h-full w-full flex-col">
+            <div className="flex h-full w-full flex-wrap justify-center gap-4">
               <Form>
                 {({ submit }) => (
-                  <Field name="icon" initialValue={data?.icon} ref={iconRef}>
-                    {({ value, setValue, isDirty }) => (
-                      <div className="flex flex-col">
-                        <label
-                          className={`font-bold tracking-wide text-sm ${
-                            isDirty && "text-success"
-                          }`}
-                        >
-                          Icon
-                        </label>
-                        <IconSelectDialog handleIconChange={handleIconChange}>
-                          <button
-                            className={`btn-outline btn-square btn ${
-                              isDirty && "btn-success"
+                  <React.Fragment>
+                    <div className="flex flex-col">
+                      <Field
+                        name="icon"
+                        initialValue={data?.icon}
+                        ref={iconRef}
+                      >
+                        {({ value, setValue, isDirty }) => (
+                          <div className="flex flex-col">
+                            <label
+                              className={`font-bold tracking-wide text-sm ${
+                                isDirty && "text-success"
+                              }`}
+                            >
+                              Icon
+                            </label>
+                            <IconSelectDialog
+                              handleIconChange={handleIconChange}
+                            >
+                              <button
+                                className={`btn-outline btn-square btn ${
+                                  isDirty && "btn-success"
+                                }`}
+                              >
+                                <IconDisplay icon={value} />
+                              </button>
+                            </IconSelectDialog>
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="title" initialValue={data.title}>
+                        {({ value, setValue, isDirty }) => (
+                          <div className="flex flex-col">
+                            <label
+                              className={`font-bold tracking-wide text-sm ${
+                                isDirty && "text-success"
+                              }`}
+                            >
+                              Title
+                            </label>
+                            <input
+                              className={`input-bordered input ${
+                                isDirty && "input-success"
+                              }`}
+                              value={value}
+                              onChange={(e) => setValue(e.target.value)}
+                            />
+                          </div>
+                        )}
+                      </Field>
+                    </div>
+                    <Field
+                      name="description"
+                      initialValue={data.shortDescription}
+                    >
+                      {({ value, setValue, isDirty }) => (
+                        <div className="flex flex-col">
+                          <label
+                            className={`font-bold tracking-wide text-sm ${
+                              isDirty && "text-success"
                             }`}
                           >
-                            <IconDisplay icon={value} />
-                          </button>
-                        </IconSelectDialog>
-                      </div>
-                    )}
-                  </Field>
+                            Description
+                          </label>
+                          <textarea
+                            className={`textarea-bordered textarea  resize-none scrollbar-thin ${
+                              isDirty && "input-success"
+                            }`}
+                            value={value}
+                            onChange={(e) => setValue(e.target.value)}
+                            rows={5}
+                            maxLength={150}
+                          />
+                          {value.length}/150 Max characters
+                        </div>
+                      )}
+                    </Field>
+                    <Field
+                      name="primaryImage"
+                      initialValue={data.primaryImage.public_id}
+                      ref={primaryImageRef}
+                    >
+                      {({ value, setValue, isDirty }) => (
+                        <div className="flex flex-col">
+                          <label
+                            className={`font-bold tracking-wide text-sm ${
+                              isDirty && "text-success"
+                            }`}
+                          >
+                            Primary Image
+                          </label>
+                          <ImageSelectDialog
+                            position="primary"
+                            handleImageChange={handleImageChange}
+                          >
+                            <button className="btn-outline btn-square btn h-fit w-fit p-6">
+                              <div className="overflow-hidden">
+                                <CldImage
+                                  src={
+                                    env.NEXT_PUBLIC_CLOUDINARY_FOLDER +
+                                    "/" +
+                                    value
+                                  }
+                                  alt="Primary Image"
+                                  width={158}
+                                  height={158}
+                                  className="rounded-xl object-center transition-all hover:scale-110"
+                                  crop="thumb"
+                                  placeholder="blur"
+                                  blurDataURL={data.primaryImage.blur_url}
+                                />
+                              </div>
+                            </button>
+                          </ImageSelectDialog>
+                        </div>
+                      )}
+                    </Field>
+                    <Field
+                      name="secondaryImage"
+                      initialValue={data.secondaryImage.public_id}
+                      ref={secondaryImageRef}
+                    >
+                      {({ value, setValue, isDirty }) => (
+                        <div className="flex flex-col">
+                          <label
+                            className={`font-bold tracking-wide text-sm ${
+                              isDirty && "text-success"
+                            }`}
+                          >
+                            Secondary Image
+                          </label>
+                          <ImageSelectDialog
+                            position="secondary"
+                            handleImageChange={handleImageChange}
+                          >
+                            <button className="btn-outline btn-square btn h-fit w-fit p-6 ">
+                              <div className="overflow-hidden rounded-xl">
+                                <CldImage
+                                  src={
+                                    env.NEXT_PUBLIC_CLOUDINARY_FOLDER +
+                                    "/" +
+                                    value
+                                  }
+                                  alt="Secondary Image"
+                                  width={158}
+                                  height={158}
+                                  className="rounded-xl object-center transition-all hover:scale-110 "
+                                  crop="thumb"
+                                  placeholder="blur"
+                                  blurDataURL={data.secondaryImage.blur_url}
+                                />
+                              </div>
+                            </button>
+                          </ImageSelectDialog>
+                        </div>
+                      )}
+                    </Field>
+                  </React.Fragment>
                 )}
               </Form>
             </div>
