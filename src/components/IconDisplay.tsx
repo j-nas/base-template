6687c0b1fa9d29a777a78d3d1ef5f8faf7ad1e5e
Jon Naske <jonnaske@hotmail.com>
2023-03-31 09:03:22 -0700

    🚨 service editor

--- src/components/IconDisplay.tsx
+++ src/components/IconDisplay.tsx
@@ -1,13 +1,13 @@
 import * as icons from "react-icons/fa";
 
-type IconList = keyof typeof icons;
+export type IconList = keyof typeof icons;
 
 type Props = {
   icon: IconList;
 };
 
 export default function IconDisplay({ icon }: Props) {
-  const Icon = icon ? icons[icon as IconList] : null;
+  const Icon = icon ? icons[icon] : null;
 
   return <span>{Icon && <Icon className="h-6 w-6" />}</span>;
 }
