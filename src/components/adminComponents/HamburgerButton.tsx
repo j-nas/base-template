332bc6847f87ee1b84fd2f3f9efe937fff2d95cb
Jon Naske <jonnaske@hotmail.com>
2023-03-31 09:20:11 -0700

    💄 navbar

--- src/components/adminComponents/HamburgerButton.tsx
+++ src/components/adminComponents/HamburgerButton.tsx
@@ -8,15 +8,15 @@ export default function HamburgerButton({ toggleSidebar, sidebarOpen }: Props) {
     <>
       <label
         aria-label="nav menu"
-        className={`swap-rotate swap disabled btn-circle btn  pl-4 md:hidden`}
+        className={`swap-rotate swap disabled btn-md btn-circle btn scale-90 pl-4 md:hidden`}
       >
         <input type="checkbox" checked={sidebarOpen} onChange={toggleSidebar} />
 
         <svg
           className="swap-off -translate-x-2 fill-current"
           xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
+          width="24"
+          height="24"
           viewBox="0 0 512 512"
         >
           <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
@@ -25,8 +25,8 @@ export default function HamburgerButton({ toggleSidebar, sidebarOpen }: Props) {
         <svg
           className="swap-on -translate-x-2 fill-current"
           xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
+          width="24"
+          height="24"
           viewBox="0 0 512 512"
         >
           <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
