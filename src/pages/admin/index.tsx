2623a863180857873b66b4c39991de49c2f24d90
Jon Naske <jonnaske@hotmail.com>
2023-04-01 16:44:07 -0700

    💬 dashboard home updated

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,25 +3,61 @@ import { type ReactElement } from "react";
 import Layout from "../../components/adminComponents/Layout";
 import { type NextPageWithLayout } from "../_app";
 import Breadcrumbs from "../../components/adminComponents/Breadcrumbs";
+import { useSession } from "next-auth/react";
 
 export const AdminHome: NextPageWithLayout = () => {
+  const { data: session } = useSession();
   return (
-    <section className=" relative  flex h-full w-full flex-col   place-items-center ">
+    <section className=" relative flex  h-full w-full flex-col place-items-center   pb-12 ">
       <div className="w-full">
         <Breadcrumbs />
       </div>
-      <div className="container prose mt-20 h-full place-self-center px-6">
+      <div className="container prose mt-20 h-full place-self-center px-6 pb-12">
         <h1>Dashboard Home</h1>
         <p>
-          Welcome to your self serve dashboard. Here you can manage the content
-          of your site, including <Link href="admin/images">images</Link>,{" "}
+          Welcome the self serve dashboard. Here you can manage site content
+          like of your site, including <Link href="admin/images">images</Link>,{" "}
           <Link href="admin/blog">blog posts</Link>, and your{" "}
-          <Link href="admin/business">business profile.</Link> Choose a section
-          from the menu on the left on desktop, or if you&apos;re on mobile,
-          then press the button at the top to access the menu. to edit.
-          Additionally, you can add contributors to the site, and manage your
-          uploaded images.
+          <Link href="admin/user/me">user profile.</Link>
         </p>
+        <ul>
+          <li>
+            <Link href="/admin/images">Image Management</Link>
+          </li>
+          {session?.user?.admin && (
+            <>
+              <li>
+                <Link href="/admin/business">Business Profile</Link>
+              </li>
+              <li>
+                <Link href="/admin/services">Services</Link>
+              </li>
+              <li>
+                <Link href="/admin/about">About Us</Link>
+              </li>
+              <li>
+                <Link href="/admin/testimonials">Testimonial Management</Link>
+              </li>
+              <li>
+                <Link href="/admin/hero">Hero Banners</Link>
+              </li>
+              <li>
+                <Link href="/admin/gallery">Gallery Editor</Link>
+              </li>
+            </>
+          )}
+          <li>
+            <Link href="/admin/blog">Blogs</Link>
+          </li>
+          {session?.user?.superAdmin && (
+            <li>
+              <Link href="/admin/user">User Management</Link>
+            </li>
+          )}
+          <li>
+            <Link href="/admin/user/me">My Profile</Link>
+          </li>
+        </ul>
         <p>
           If you need help or instructions, you can{" "}
           <a href="https://shorecel.com/support">click here</a> to view our
