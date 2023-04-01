d690e4b9e69555128418ac0d0f14c2eb8946b083
Jon Naske <jonnaske@hotmail.com>
2023-04-01 11:26:35 -0700

    ✨ log in page implemented

--- src/components/adminComponents/Login.tsx
+++ src/components/adminComponents/Login.tsx
@@ -1,14 +1,66 @@
+import Logo from "@/Logo";
+import { Form, Field } from "houseform";
+import { z } from "zod";
+import { signIn } from "next-auth/react";
+
 export default function Login() {
   return (
-    <div className="grid h-full w-full place-content-center place-items-center overflow-auto">
-      <div>
-        <form>
-          <div className="form-control gap-4">
-            <input className="input-primary input"></input>
-            <input className="input-primary input"></input>
-          </div>
-        </form>
+    <div className="flex h-screen  flex-col ">
+      <div className="">
+        <Logo className="mask mx-auto w-72   fill-base-content stroke-base-content" />
+        <Form
+          onSubmit={(values) =>
+            signIn("email", { email: values.email as string, redirect: true })
+          }
+        >
+          {({ errors, submit }) => (
+            <div className="form-control place-items-center gap-4 place-self-center">
+              <Field<string>
+                name="email"
+                onChangeValidate={z
+                  .string()
+                  .min(1, "Cannot be blank")
+                  .email("Please enter a valid email")}
+              >
+                {({ value, setValue, onBlur }) => (
+                  <div className="flex flex-col">
+                    <label className={`font-bold tracking-wide text-sm `}>
+                      Email
+                    </label>
+                    <input
+                      value={value}
+                      type="email"
+                      autoComplete="off"
+                      className="input input-primary"
+                      onBlur={onBlur}
+                      onChange={(e) => setValue(e.target.value)}
+                    ></input>
+                  </div>
+                )}
+              </Field>
+              <button
+                className={`btn ${
+                  errors.length > 0 ? "btn-disabled btn-error" : "btn-primary"
+                }`}
+                onClick={submit}
+              >
+                Login with email
+              </button>
+              {errors.map((e) => (
+                <div key={e} className="text-error">
+                  {e}
+                </div>
+              ))}
+            </div>
+          )}
+        </Form>
       </div>
+      <span className="mt-32 place-self-center text-center">
+        <p>Trouble logging in?</p>
+        <a className="link" href="mailto://support@shorecel.com">
+          Email support
+        </a>
+      </span>
     </div>
   );
 }
