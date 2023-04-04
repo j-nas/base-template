ee481dd9d0e6ecbf08e3f970c42bccd993a5f031
Jon Naske <jonnaske@hotmail.com>
2023-04-04 14:38:26 -0700

    🚧 og image function

---
+++ netlify/functions/generator.ts
@@ -0,0 +1,56 @@
+import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+import fs from "fs";
+import type { HandlerEvent, HandlerContext, HandlerResponse } from '@netlify/functions'
+
+export const handler = async (_event: HandlerEvent, _context: HandlerContext): Promise<HandlerResponse> => {
+  try {
+    const localChrome =
+      "/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe";
+    const executable = fs.existsSync(localChrome)
+      ? localChrome
+      : await chromium.executablePath;
+    console.log(executable)
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: executable,
+      headless: true,
+
+      defaultViewport: { height: 630, width: 1200 },
+    });
+    console.log("hello")
+
+    const page = await browser.newPage();
+
+    const content = fs.readFileSync(__dirname + "/assets/image.html").toString();
+
+
+
+    await page.setContent(content, { waitUntil: "domcontentloaded" });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "image/png",
+        "Cache-Control": "s-maxage=31536000",
+      },
+      body: await page.screenshot({ type: "png", encoding: "base64" }) as string,
+      isBase64Encoded: true,
+    };
+  } catch (e) {
+    console.log(e)
+    const error = e as Error;
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/json",
+        "Cache-Control": "s-maxage=31536000",
+      },
+      body: error.message,
+
+
+    };
+  }
+
+};
+
