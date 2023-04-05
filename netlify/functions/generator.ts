781aa664c590c9c914db370db31df3e0172295d1
Jon Naske <jonnaske@hotmail.com>
2023-04-05 12:42:25 -0700

    ✨ default flag for og gen, gets  unsplash image

--- netlify/functions/generator.ts
+++ netlify/functions/generator.ts
@@ -2,12 +2,16 @@ import chromium from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
 import fs from "fs";
 import { type Handler, type HandlerEvent, type HandlerContext, type HandlerResponse } from '@netlify/functions'
+import { env } from "~/env/client.mjs";
 
 type Data = {
   title?: string;
   subtitle?: string;
   image?: string;
+  default?: "true"
 };
+
+const cloudinaryUrl = `https://res.cloudinary.com/dkascnwj7/image/upload/c_fill,h_630,w_700/${env.NEXT_PUBLIC_CLOUDINARY_FOLDER}/`
 
 export const handler: Handler = async (event: HandlerEvent, _context: HandlerContext): Promise<HandlerResponse> => {
   try {
@@ -26,16 +30,20 @@ export const handler: Handler = async (event: HandlerEvent, _context: HandlerCon
 
     const page = await browser.newPage();
 
+
+
     let content = fs.readFileSync("netlify/functions/assets/image.html").toString();
 
     content = populateTemplate(content, {
       title: event.queryStringParameters?.title,
-      subtitle: event.queryStringParameters?.subtitle,
-      image: event.queryStringParameters?.image
+      subtitle: event.queryStringParameters?.subtitle || "",
+      image: event.queryStringParameters?.default === "true"
+        ? "https://source.unsplash.com/random/700x630/?tech"
+        : cloudinaryUrl + (event.queryStringParameters?.image as string)
     })
 
 
-    await page.setContent(content, { waitUntil: "domcontentloaded" });
+    await page.setContent(content, { waitUntil: "networkidle0" });
 
     return {
       statusCode: 200,
